88c0abd59a56d1a5c48ed44a5ffb0bc09476e748
Romulo Sousa <romulosousa27@hotmail.com>
2018-10-08 22:08:04 -0300

    namespace model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -27,4 +27,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+
+    public function commets(){
+        return $this->hasMany(Comment::class);
+    }
 }
