f87b06e2640c47f6837a886360039aa64342bd40
Romulo Sousa <romulosousa27@hotmail.com>
2018-10-11 22:55:13 -0300

    link para motrar o post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,8 @@
 
     <h1>Posts</h1>
     @forelse ($posts as $post)
-        {{$post->title}}
+            {{ $post->title }}
+            <a href="{{ route('posts.show', $post->id )}}">ver</a>
         <hr>
     @empty
         <p>Nenhuma Post Cadastrado</p>
