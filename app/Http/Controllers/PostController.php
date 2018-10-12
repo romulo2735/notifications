b2356911405c024739fe300ecbb2671e5e35a5e9
Romulo Sousa <romulosousa27@hotmail.com>
2018-10-11 22:54:30 -0300

    Metodo show() __construct() para faciliar a injeção da model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,9 +8,19 @@ use App\Models\Post;
 class PostController extends Controller
 {
 
+    public function __construct(Post $post){
+        $this->post = $post;
+    }
+
     public function index(){
         $posts = Post::paginate();
 
         return view('posts.index', compact('posts'));
+    }
+
+    public function show($id){
+        $post = $this->post->findOrFail($id);
+
+        return view('posts.show', compact('post'));
     }
 }
