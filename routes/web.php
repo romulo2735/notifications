c01f6d8bf93428b87b8489cdb98fac7c7bb3661b
Romulo Sousa <romulosousa27@hotmail.com>
2018-11-17 00:18:58 -0300

    rota store comentários

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('posts', 'PostController');
+Route::post('comment', 'CommentController@store')->name('comment.store');
