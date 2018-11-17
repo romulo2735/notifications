7db9c40d432cc8b6d1903ab2d03114aa9d0b5a67
Romulo Sousa <romulosousa27@hotmail.com>
2018-11-17 00:20:05 -0300

    renomeando o campo de conteudo do comentario

--- database/migrations/2018_10_08_212109_create_comments_table.php
+++ database/migrations/2018_10_08_212109_create_comments_table.php
@@ -16,7 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->text('content');
+            $table->text('body');
 
             $table->unsignedInteger('post_id');
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
