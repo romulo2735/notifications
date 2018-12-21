d902299a1bd4276f0b757683572e0e3a42556ea5
Romulo <romulosousa27@hotmail.com>
2018-12-21 20:38:27 -0300

    listando os comentarios os posts e contando

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,19 +7,33 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-
+    private $post;
+    /**
+     * PostController constructor.
+     * @param Post $post
+     */
     public function __construct(Post $post){
         $this->post = $post;
     }
 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index(){
-        $posts = Post::paginate();
+        $posts = $this->post->with('comments')->paginate();
 
         return view('posts.index', compact('posts'));
     }
 
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show($id){
-        $post = $this->post->findOrFail($id);
+
+        /** @var  $post */
+        // Retornando o user que fez o comentário e o user dono do Post.
+        $post = $this->post->with('comments.user', 'user')->findOrFail($id);
 
         return view('posts.show', compact('post'));
     }
